959aa0f275c181e147c511e23e621da18d146e8b
Caio <caiooliv97@gmail.com>
2020-10-23 22:15:56 -0300

    Criada versao inicial do chat

--- chat-front/src/App.js
+++ chat-front/src/App.js
@@ -1,12 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
+import SendIcon from '@material-ui/icons/Send';
+
 
 function App() {
   return (
-    <div >
-      <div >
-        <h1>Chat</h1>
+    <div style={{display:"flex",justifyContent:"center",alignItems:"center", flex:1}}>
+      <div style={{height:600,width:'70%', backgroundColor:'#e5ddd5', display:"flex", flexDirection:"column"}} >
+        <AppBar position="sticky"  style={{backgroundColor:'#3dd164'}}>
+          <Typography variant="h3" >
+            Chat do huff
+          </Typography>        
+        </AppBar>
+        
+        <div style={{display:"flex", flex:1, alignItems:"center", flexDirection:"column", overflow:"scroll"}}>
+        <p>dsknfsdkljf</p>
+        <p>fdskoif</p>
+        <p>dsknfsdkljf</p>
+        <p>fdskoif</p>  <p>dsknfsdkljf</p>
+        <p>fdskoif</p>  <p>dsknfsdkljf</p>
+        <p>fdskoif</p>  <p>dsknfsdkljf</p>
+        <p>fdskoif</p>  <p>dsknfsdkljf</p>
+        <p>fdskoif</p>  <p>dsknfsdkljf</p>
+        <p>fdskoif</p>  <p>dsknfsdkljf</p>
+        <p>fdskoif</p>  <p>dsknfsdkljf</p>
+        <p>fdskoif</p>  <p>dsknfsdkljf</p>
+        <p>fdskoif</p>  <p>dsknfsdkljf</p>
+        <p>fdskoif</p>  <p>dsknfsdkljf</p>
+        <p>fdskoif</p>  <p>dsknfsdkljf</p>
+        <p>fdskoif</p>
+        </div>
+        
+        
+        <div style={{display:"flex",alignSelf:"end" ,width:"100%"}}>
+          <TextField  style={{width:"100%"}} variant="outlined" /> 
+          <IconButton color="primary" >
+            <SendIcon style={{color:'#3dd164'}}/>
+          </IconButton>
+
+        </div>
       </div>
     </div>
   );
