493784e337bc0c356a8599b5eb0d696b14e4f8a5
mrodrigues <matheus.1nascimento58@gmail.com>
2020-10-23 22:35:28 -0300

    conectando ao socket
    Co-authored-by: Caio <caiooliv97@gmail.com>
    Signed-off-by: mrodrigues <matheus.1nascimento58@gmail.com>

--- chat-front/src/App.js
+++ chat-front/src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState, useCallback } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField';
 import IconButton from '@material-ui/core/IconButton';
 import SendIcon from '@material-ui/icons/Send';
+import socketIOClient from "socket.io-client";
 
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [text, setText] = useState('');
+  const socket = socketIOClient('http://localhost:8001/chat');
+    
+  useEffect(()=>{
+    socket.on('receivedMessage', (message)=>{
+      setMessages(prevState=> [...prevState, message]);
+    });
+  }, [])
+
+  const handleChange = useCallback(()=>{
+    socket.emit('chatMessage', text);
+    setText('');
+  },[text])
+
   return (
     <div style={{display:"flex",justifyContent:"center",alignItems:"center", flex:1}}>
       <div style={{height:600,width:'70%', backgroundColor:'#e5ddd5', display:"flex", flexDirection:"column"}} >
@@ -18,27 +33,14 @@ function App() {
         </AppBar>
         
         <div style={{display:"flex", flex:1, alignItems:"center", flexDirection:"column", overflow:"scroll"}}>
-        <p>dsknfsdkljf</p>
-        <p>fdskoif</p>
-        <p>dsknfsdkljf</p>
-        <p>fdskoif</p>  <p>dsknfsdkljf</p>
-        <p>fdskoif</p>  <p>dsknfsdkljf</p>
-        <p>fdskoif</p>  <p>dsknfsdkljf</p>
-        <p>fdskoif</p>  <p>dsknfsdkljf</p>
-        <p>fdskoif</p>  <p>dsknfsdkljf</p>
-        <p>fdskoif</p>  <p>dsknfsdkljf</p>
-        <p>fdskoif</p>  <p>dsknfsdkljf</p>
-        <p>fdskoif</p>  <p>dsknfsdkljf</p>
-        <p>fdskoif</p>  <p>dsknfsdkljf</p>
-        <p>fdskoif</p>  <p>dsknfsdkljf</p>
-        <p>fdskoif</p>  <p>dsknfsdkljf</p>
-        <p>fdskoif</p>
+        {messages.map((message, index)=>(
+          <p key={String(index)}>{message}</p>
+        ))}
         </div>
         
-        
         <div style={{display:"flex",alignSelf:"end" ,width:"100%"}}>
-          <TextField  style={{width:"100%"}} variant="outlined" /> 
-          <IconButton color="primary" >
+          <TextField  style={{width:"100%"}} variant="outlined" value={text} onChange={(value)=> setText(value.target.value)}/> 
+          <IconButton color="primary" onClick={handleChange}>
             <SendIcon style={{color:'#3dd164'}}/>
           </IconButton>
 
